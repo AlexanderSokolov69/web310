15bc541985c800ee04b00dd0f672b4a87d31ea39
AlexanderSokolov69 <sokoloval@yandexlyceum.ru>
2021-11-03 14:23:14 +0300

    Work.

--- main.py
+++ main.py
@@ -14,6 +14,7 @@ from data.db_class_priv import Priv
 from data.db_class_rasp import Rasp
 from data.db_class_roles import Roles
 from data.db_class_users import Users
+from forms.f_rasp import RaspFilterForm
 from forms.f_user import LoginForm, RegisterForm
 
 app = Flask(__name__)
@@ -21,10 +22,15 @@ app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
 login_manager = LoginManager()
 login_manager.init_app(app)
 
-
 @app.errorhandler(404)
+@app.errorhandler(401)
 def not_found(error):
-    return render_template("error404.html")
+    # session.year = Const.YEAR
+    if "401" in str(error):
+        msg = 'Пользователь не авторизован (401)'
+    elif "404" in str(error):
+        msg = 'Страница не найдена (404)'
+    return render_template("error404.html", message=msg)
 
 
 @login_manager.user_loader
@@ -32,21 +38,71 @@ def load_user(user_id):
     db_sess = db_session.create_session()
     return db_sess.query(Users).get(user_id)
 
+@app.route('/rasp', methods=['GET', 'POST'])
+def free_view():
+    db_sess = db_session.create_session()
+    rsp = db_sess.query(Rasp).join(Groups).join(Users).order_by(Rasp.idDays, Rasp.tstart, Rasp.idKabs)
+    form = RaspFilterForm()
+    dat = rsp
+    if form.validate_on_submit():
+        #    if request.method == 'POST':
+        if form.f_users.data != 0:
+            dat = dat.filter(Groups.idUsers == form.f_users.data)
+        if form.f_weekday.data != 0:
+            dat = dat.filter(Rasp.idDays == form.f_weekday.data)
+        if form.f_kabinet.data != 0:
+            dat = dat.filter(Rasp.idKabs == form.f_kabinet.data)
+        if form.f_course.data != 0:
+            dat = dat.filter(Groups.idCourses == form.f_course.data)
+    cnt = dat.count()
+    return render_template("rasp_view_free.html", items=dat, form=form, cnt=cnt)
+
+@app.route('/', methods=['GET', 'POST'])
+def base():
+    # if not session.get('year'):
+        # session.year = Const.YEAR
+    return render_template("base.html")
+
+
 
 @app.route('/login', methods=['GET', 'POST'])
-@app.route('/', methods=['GET', 'POST'])
 def login():
     form = LoginForm()
     if form.validate_on_submit():
         db_sess = db_session.create_session()
-        user = db_sess.query(Users).filter(Users.login == form.login.data).first()
+        user = db_sess.query(Users).join(Roles).join(Priv).filter(Users.login == form.login.data).first()
         if user and user.check_password(form.password.data):
             login_user(user, remember=form.remember_me.data)
+            session.current_user = user
+            session.year = Const.YEAR
             return redirect("/base")
         return render_template('login.html',
                                message="Неправильный логин или пароль",
                                form=form)
     return render_template('login.html', title='Авторизация', form=form)
+
+
+@app.route("/main", methods=['GET', 'POST'])
+@login_required
+def index():
+#    if request.method == 'GET':
+    db_sess = db_session.create_session()
+    rsp = db_sess.query(Rasp).join(Groups).join(Users).order_by(Rasp.idDays, Rasp.tstart, Rasp.idKabs)
+    form = RaspFilterForm()
+    dat = rsp
+    print(*session)
+    if form.validate_on_submit():
+#    if request.method == 'POST':
+        if form.f_users.data != 0:
+            dat = dat.filter(Groups.idUsers == form.f_users.data)
+        if form.f_weekday.data != 0:
+            dat = dat.filter(Rasp.idDays == form.f_weekday.data)
+        if form.f_kabinet.data != 0:
+            dat = dat.filter(Rasp.idKabs == form.f_kabinet.data)
+        if form.f_course.data != 0:
+            dat = dat.filter(Groups.idCourses == form.f_course.data)
+    cnt = dat.count()
+    return render_template("rasp_view.html", items=dat, form=form, cnt=cnt)
 
 
 @app.route('/register', methods=['GET', 'POST'])
@@ -85,14 +141,6 @@ def base_view():
     return render_template('base.html')
 
 
-@app.route("/main")
-@login_required
-def index():
-    db_sess = db_session.create_session()
-    rsp = db_sess.query(Rasp).order_by(Rasp.idDays, Rasp.tstart, Rasp.idKabs).all()
-    return render_template("rasp_view.html", items=rsp)
-
-
 @app.route("/rasp")
 def rasp_view():
     db_sess = db_session.create_session()
@@ -102,7 +150,8 @@ def rasp_view():
 
 def main():
     db_session.global_init("DSN=it-cube64")
-    app.run()
+    app.run(host='0.0.0.0')
+    
 
 
 if __name__ == '__main__':
